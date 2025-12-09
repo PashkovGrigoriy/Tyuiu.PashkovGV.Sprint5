1a90f7b8dae67e8aa639c99431cc79c41071721c
PashkovGrigoriy <gripas2006@gmail.com>
2025-12-09 22:46:03 +0500

    T3

---
+++ Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib
+{
+    public class DataService : ISprint5Task3V1
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string r = Path.GetTempPath();
+            string p = Path.Combine(r, "OutPutFileTask3.bin");
+            double z = (Math.Pow(x, 3) - 8 )/ (2 * Math.Pow(x, 2));
+            z = Math.Round(z);
+            using(BinaryWriter w = new BinaryWriter(File.Open(p, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                w.Write(BitConverter.GetBytes(z));
+            }
+            return(p);
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint5.Task3.V1/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib;
+namespace Tyuiu.PashkovGV.Sprint5.Task3.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int x = 3;
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
