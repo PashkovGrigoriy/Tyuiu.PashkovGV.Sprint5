c21f54a384f4c033f19f7e74029a3181ff6f2056
PashkovGrigoriy <gripas2006@gmail.com>
2025-12-09 22:47:47 +0500

    T3

--- Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib/DataService.cs
+++ Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib/DataService.cs
@@ -8,8 +8,8 @@ namespace Tyuiu.PashkovGV.Sprint5.Task3.V1.Lib
         {
             string r = Path.GetTempPath();
             string p = Path.Combine(r, "OutPutFileTask3.bin");
-            double z = (Math.Pow(x, 3) - 8 )/ (2 * Math.Pow(x, 2));
-            z = Math.Round(z);
+            double z = ((Math.Pow(x, 3)) - 8 )/ (2 * (Math.Pow(x, 2)));
+            z = Math.Round(z,3);
             using(BinaryWriter w = new BinaryWriter(File.Open(p, FileMode.OpenOrCreate), Encoding.UTF8))
             {
                 w.Write(BitConverter.GetBytes(z));
