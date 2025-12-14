ac05e79bbd5f4f1a36d78f60706b8215195dfc23
PashkovGrigoriy <gripas2006@gmail.com>
2025-12-15 04:19:50 +0500

    T7

---
+++ Tyuiu.PashkovGV.Sprint5.Task7.V18.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.PashkovGV.Sprint5.Task7.V18.Lib
+{
+    public class DataService : ISprint5Task7V18
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string s = File.ReadAllText(path);
+            s = s.Replace("н", "нн");
+            return s;
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint5.Task7.V18/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PashkovGV.Sprint5.Task7.V18.Lib;
+namespace Tyuiu.PashkovGV.Sprint5.Task7.V18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            string a = @"D:\DataSprint5";
+            string b = "InPutDataFileTask7V18.txt";
+
+            string f = Path.Combine(a, b);
+
+            string g = ds.LoadDataAndSave(f);
+            File.WriteAllText(f, g);
+            Console.ReadKey();
+        }
+    }
+}
